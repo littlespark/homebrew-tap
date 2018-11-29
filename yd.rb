4434d0433542fd7e53dcc8c4050af4dd7a125190
littlespark <zfx186@gmail.com>
2018-11-29 11:59:16 +0800

    Update yd.rb

--- yd.rb
+++ yd.rb
@@ -1,7 +1,7 @@
 class Yd < Formula
   desc "有道翻译go版本"
   homepage "https://github.com"
-  url "https://github.com/littlespark/yd/archive/V1.0.tar.gz"
+  url "https://github.com/littlespark/yd/releases/download/V1.0/yd-darwin-amd64.tar.gz"
   sha256 "2648dfe1f06d6d70f0df2a52dfe2a2fbdbf66cb271d35de6eb12e4807e0e6476"
 
   def install
